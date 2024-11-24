5ff029c5757bffc8a72a831ab23a88f1863f5616
kenkomu <komukenneth216@gmail.com>
2024-11-24 10:34:49 +0300

    solve errors

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -11,13 +11,41 @@ export default function RootLayout({
 
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn} />
+      <Sidebar user={{
+        $id: "",
+        email: "",
+        userId: "",
+        dwollaCustomerUrl: "",
+        dwollaCustomerId: "",
+        firstName: "",
+        lastName: "",
+        address1: "",
+        city: "",
+        state: "",
+        postalCode: "",
+        dateOfBirth: "",
+        ssn: ""
+      }}/>
 
       <div className="flex size-full flex-col">
         <div className="root-layout">
           <Image src="/icons/logo.png" width={30} height={30} alt="logo" />
           <div>
-            <MobileNav user={loggedIn} />
+            <MobileNav user={{
+              $id: "",
+              email: "",
+              userId: "",
+              dwollaCustomerUrl: "",
+              dwollaCustomerId: "",
+              firstName: "",
+              lastName: "",
+              address1: "",
+              city: "",
+              state: "",
+              postalCode: "",
+              dateOfBirth: "",
+              ssn: ""
+            }}/>
           </div>
         </div>
         {children}
