8d45e0a2fd67f6ab2c74c014b6c8479b4c698d61
kenkomu <komukenneth216@gmail.com>
2024-11-24 11:40:14 +0300

    solve errors

--- frontend/pages/api/create-card.ts
+++ frontend/pages/api/create-card.ts
@@ -1,9 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2022-11-15',
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', );
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
