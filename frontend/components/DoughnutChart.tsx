7dccf92c1361d917dc9890cd87327a03d2371298
kenkomu <komukenneth216@gmail.com>
2024-11-24 10:09:24 +0300

    solved errors

--- frontend/components/DoughnutChart.tsx
+++ frontend/components/DoughnutChart.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { DoughnutChartProps } from "@/types";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
--- frontend/components/TotalBalanceBox.tsx
+++ frontend/components/TotalBalanceBox.tsx
@@ -1,3 +1,4 @@
+import { TotlaBalanceBoxProps } from '@/types';
 import AnimatedCounter from './AnimatedCounter';
 import DoughnutChart from './DoughnutChart';
 
