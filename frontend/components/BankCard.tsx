2347dbb0c6baa21804ab2d9632f4c17e9489835f
kenkomu <komukenneth216@gmail.com>
2024-11-24 10:07:08 +0300

    configured the bankcard file

--- frontend/components/BankCard.tsx
+++ frontend/components/BankCard.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const BankCard = ({userName, cardNumber, expiration = true }) => {
+interface BankCardProps {
+  userName: string;
+  cardNumber?: string;
+  expiration?: boolean;
+}
+
+const BankCard: React.FC<BankCardProps> = ({userName, cardNumber, expiration = true }) => {
   return (
     <div className="flex flex-col">
       <div className="bank-card">
@@ -10,14 +16,13 @@ const BankCard = ({userName, cardNumber, expiration = true }) => {
               {userName}
             </h1>
           </div>
-
           <article className="flex flex-col gap-2">
             <div className="flex justify-between">
               <h1 className="text-12 font-semibold text-white">
                 {userName}
               </h1>
               <h2 className="text-12 font-semibold text-white">
-                {expiration || '●● / ●●'}  {/* Show expiration if available */}
+                {expiration || '●● / ●●'} {/* Show expiration if available */}
               </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
@@ -25,7 +30,6 @@ const BankCard = ({userName, cardNumber, expiration = true }) => {
             </p>
           </article>
         </div>
-
         <div className="bank-card_icon">
           {/* Assuming static icons here */}
         </div>
